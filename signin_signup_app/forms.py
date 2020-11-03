69bac7929b9be6c62eeb3e76a6d80bf3543a9e2b
VGT GAYAN <gayanvgt@gmail.com>
2020-11-03 12:17:36 +0530

    created two forms for signin and signup

--- signin_signup_app/forms.py
+++ signin_signup_app/forms.py
@@ -2,12 +2,23 @@ from django import forms
 
 from .models import User
 
-class UserForm(forms.ModelForm):
+class UserSignupForm(forms.ModelForm):
     class Meta:
         model = User
         fields = [
             'name',
             'email',
+            'password'
+        ]
+        widgets = {
+            'password' : forms.PasswordInput(),
+        }
+    
+class UserSigninForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = [
+            'name',
             'password'
         ]
         widgets = {
--- signin_signup_app/views.py
+++ signin_signup_app/views.py
@@ -1,16 +1,19 @@
 from django.shortcuts import render
-from .forms import UserForm
+from .forms import (
+                UserSignupForm,
+                UserSigninForm
+)
 
 # How to use 2 forms in one html template? How will they handle POST request?
 def signin_view(request):
-    form = UserForm(request.POST or None)
-    if form.is_valid():
-        form.save()
-        form = UserForm()
+    signup_form = UserSignupForm(request.POST or None)
+    if signup_form.is_valid():
+        signup_form.save()
+        signup_form = UserSignupForm()
     
     
     context = {
-        'form' : form
+        'signup_form' : signup_form
     }
 
     return render(request, 'signin_signup_app/index.html', context)
