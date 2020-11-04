d23a3f86b902ad3925059e15454b64daff1f5b6c
VGT GAYAN <gayanvgt@gmail.com>
2020-11-04 09:11:09 +0530

    Inpage warning in signin form for invalid user

--- signin_signup_app/views.py
+++ signin_signup_app/views.py
@@ -18,26 +18,30 @@ def signin_view(request):
             signup_form = UserSignupForm()
             return render(request, 'signin_signup_app/signup_page.html', {})
     
-    valid_pwd = True    
+    valid_pwd = True
+    valid_usr = True
     signin_form = UserSigninForm(request.POST or None)    
     # Used ('email' not in request.POST) to distinguish from signup form request.POST
     if signin_form.is_valid() and ('email' not in request.POST):
         try:
             user = User.objects.get(name=signin_form.cleaned_data['name'])
+            if user.password == signin_form.cleaned_data['password']:
+                # return HttpResponse("<h1>You have logged in</h1>")
+                return render(request, 'signin_signup_app/signin_page.html', {})
+            else:
+                # return HttpResponse("<h1>Incorrect Password</h1>")
+                valid_pwd = False
         except User.DoesNotExist:
-            return redirect('/')
-        if user.password == signin_form.cleaned_data['password']:
-            # return HttpResponse("<h1>You have logged in</h1>")
-            return render(request, 'signin_signup_app/signin_page.html', {})
-        else:
-            # return HttpResponse("<h1>Incorrect Password</h1>")
-            valid_pwd = False
+            valid_usr = False
+            # return redirect('/')
+        
 
     
     context = {
         'signup_form' : signup_form,
         'signin_form' : signin_form,
-        'valid_pwd'   : valid_pwd
+        'valid_pwd'   : valid_pwd,
+        'valid_usr'   : valid_usr
     }
 
     return render(request, 'signin_signup_app/index.html', context)
