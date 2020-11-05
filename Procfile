743dcd4be5f850d4481ed70cf69f3051720f3b3f
VGT GAYAN <gayanvgt@gmail.com>
2020-11-05 20:08:27 +0530

    Tweaked app for heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn signin_signup_prj.wsgi
--- signin_signup_prj/settings.py
+++ signin_signup_prj/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 
 import os
 
+import django_heroku
+import dj_database_url
+from decouple import config
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -50,6 +54,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'signin_signup_prj.urls'
@@ -125,3 +130,5 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
             os.path.join(BASE_DIR, 'static')
         ]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+django_heroku.settings(locals())
